2c348ab77f2609210908f131fec24db88a4982ae
nlokantha <nlokantha@gmail.com>
2024-02-21 16:57:36 +0530

    Add Header and UI

--- app/src/main/java/com/example/weightscale/MainActivity.java
+++ app/src/main/java/com/example/weightscale/MainActivity.java
@@ -26,11 +26,11 @@ import java.text.DecimalFormat;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btn_deviceList,btn_copy,btn_minValue;
+    private Button btn_deviceList,btn_copy,btn_minValue,btn_enable;
 
     private EditText edit_minValue;
     public static BluetoothAdapter bluetoothAdapter;
-    private TextView text_device;
+    private TextView text_device,text_header;
     UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
     private BluetoothSocket mSocket;
     private BluetoothDevice mDevice;
@@ -39,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
     public static final int REQUEST_BLUETOOTH = 100;
 
     double minValue=400;
+    private boolean isSocketConnected = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +50,10 @@ public class MainActivity extends AppCompatActivity {
         initView();
 //        enableBluetooth();
         enableBlue();
+        text_header.setText("Please Select Your Weighing Scale");
         getandsetIntent();
+
+        btn_enable.setEnabled(false);
         btn_deviceList.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -57,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
 
         btn_minValue.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,7 +73,21 @@ public class MainActivity extends AppCompatActivity {
 //                    minValue = Integer.parseInt(edit_minValue.getText().toString());
                     minValue = Double.parseDouble(minVal);
                     Toast.makeText(MainActivity.this, "Done", Toast.LENGTH_SHORT).show();
+                    btn_minValue.setEnabled(false);
+                    edit_minValue.setEnabled(false);
+                    btn_enable.setEnabled(true);
+
                 }
+            }
+        });
+
+
+        btn_enable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn_minValue.setEnabled(true);
+                edit_minValue.setEnabled(true);
+                btn_enable.setEnabled(false);
             }
         });
 
@@ -78,9 +97,11 @@ public class MainActivity extends AppCompatActivity {
         btn_deviceList = findViewById(R.id.btn_deviceList);
         btn_copy = findViewById(R.id.btn_copy);
         text_device = findViewById(R.id.text_device);
+        text_header = findViewById(R.id.text_header);
 
         btn_minValue=findViewById(R.id.btn_minValue);
         edit_minValue=findViewById(R.id.edit_minValue);
+        btn_enable=findViewById(R.id.btn_enable);
     }
 
     public void enableBlue(){
@@ -98,24 +119,17 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-//    public void enableBluetooth() {
-//        if (bluetoothAdapter == null) {
-//            Toast.makeText(this, "Device Bluetooth not available", Toast.LENGTH_SHORT).show();
-//            return;
-//        }
-//        if (!bluetoothAdapter.isEnabled()) {
-//            if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED ||
-//                    ContextCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_ADMIN) != PackageManager.PERMISSION_GRANTED) {
-//                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.BLUETOOTH_ADMIN, Manifest.permission.BLUETOOTH_CONNECT}, 105);
-//            } else {
-//                Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-//                startActivityForResult(enableBluetooth, 101);
-//            }
-//        }
-//    }
-
     private void getandsetIntent() {
         if (getIntent() != null && getIntent().hasExtra("name") && getIntent().hasExtra("address")) {
+
+            if (getIntent() != null){
+                    text_header.setText("Connect With " + getIntent().getStringExtra("name"));
+            }else {
+                text_header.setText("Please Select Your Device ");
+            }
+
+
+
 //            text_device.setText(getIntent().getStringExtra("name"));
             mDevice = bluetoothAdapter.getRemoteDevice(getIntent().getStringExtra("address"));
 
@@ -130,10 +144,11 @@ public class MainActivity extends AppCompatActivity {
                     inputStream = mSocket.getInputStream();
 //                    decimalFormat = new DecimalFormat("00.00");
 //                    startReadingThread();
-                    Reading2();
+                    Reading3();
                 }
             } catch (IOException e) {
-                throw new RuntimeException(e);
+//                throw new RuntimeException(e);
+                e.printStackTrace();
             }
         }
     }
@@ -222,32 +237,102 @@ public class MainActivity extends AppCompatActivity {
                 StringBuilder receivedData = new StringBuilder();
 
                 while (true) {
-                    try {
-                        int bytesRead = inputStream.read(buffer);
-                        if (bytesRead != -1) { // Check for end of stream
-                            String dataChunk = new String(buffer, 0, bytesRead, "UTF-8");
-                            receivedData.append(dataChunk);
+                    if (mSocket.isConnected()){
+                        try {
+                            int bytesRead = inputStream.read(buffer);
+                            if (bytesRead != -1) { // Check for end of stream
+                                String dataChunk = new String(buffer, 0, bytesRead, "UTF-8");
+                                receivedData.append(dataChunk);
 
-                            // Check if the complete value is received (e.g., "04.82")
-                            if (receivedData.toString().endsWith("\n")) { // Assuming the message ends with a newline character
-                                StringBuilder finalReceivedData = receivedData;
-                                runOnUiThread(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        text_device.setText(finalReceivedData.toString().trim());
-                                        double Value = Double.parseDouble(finalReceivedData.toString().trim());
+                                // Check if the complete value is received (e.g., "04.82")
+                                if (receivedData.toString().endsWith("\n")) { // Assuming the message ends with a newline character
+                                    StringBuilder finalReceivedData = receivedData;
+                                    runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            text_device.setText(finalReceivedData.toString().trim());
+                                            double Value = Double.parseDouble(finalReceivedData.toString().trim());
 
-                                        if (Value>=minValue){
-                                            copyToClipboard(String.valueOf(finalReceivedData));
+                                            if (Value>=minValue){
+                                                copyToClipboard(String.valueOf(finalReceivedData));
+                                            }
                                         }
-                                    }
-                                });
-                                // Reset StringBuilder for the next message
-                                receivedData = new StringBuilder();
+                                    });
+                                    // Reset StringBuilder for the next message
+                                    receivedData = new StringBuilder();
+                                }
                             }
+                        } catch (IOException e) {
+                            e.printStackTrace();
                         }
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
+                    }else {
+
+                        break;
+                    }
+
+                }
+            }
+        }).start();
+    }
+
+    private void Reading3() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                byte[] buffer = new byte[1024];
+                StringBuilder receivedData = new StringBuilder();
+
+                while (isSocketConnected) {
+                    if (mSocket.isConnected()) {
+                        try {
+                            int bytesRead = inputStream.read(buffer);
+                            if (bytesRead != -1) {
+                                String dataChunk = new String(buffer, 0, bytesRead, "UTF-8");
+                                receivedData.append(dataChunk);
+
+                                if (receivedData.toString().endsWith("\n")) {
+                                    StringBuilder finalReceivedData = receivedData;
+                                    runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            text_device.setText(finalReceivedData.toString().trim());
+                                            double value = Double.parseDouble(finalReceivedData.toString().trim());
+
+                                            if (value >= minValue) {
+                                                copyToClipboard(String.valueOf(finalReceivedData));
+                                            }
+                                        }
+                                    });
+
+                                    receivedData = new StringBuilder();
+                                }
+                            }
+                        } catch (IOException e) {
+                            // Bluetooth connection is closed or dropped
+                            e.printStackTrace();
+                            isSocketConnected = false;
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    // Update UI when connection is lost
+                                    Toast.makeText(MainActivity.this, "Bluetooth Connection Lost", Toast.LENGTH_SHORT).show();
+                                    text_device.setText("00.00");
+                                    text_header.setText("Please Select Your Device");
+                                }
+                            });
+                        }
+                    } else {
+                        // Bluetooth connection is closed or dropped
+                        isSocketConnected = false;
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                // Update UI when connection is lost
+                                Toast.makeText(MainActivity.this, "Bluetooth Connection Lost", Toast.LENGTH_SHORT).show();
+                                text_device.setText("00.00");
+                                text_header.setText("Please Select Your Device");
+                            }
+                        });
                     }
                 }
             }
